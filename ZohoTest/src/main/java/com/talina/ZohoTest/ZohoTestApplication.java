1feafa00cf8869c75bd365d1d5d00d94bc0cda5f
Inna Dudnik <talina.in@gmail.com>
2022-09-10 15:14:41 +0300

    first commit

---
+++ ZohoTest/src/main/java/com/talina/ZohoTest/ZohoTestApplication.java
@@ -0,0 +1,13 @@
+package com.talina.ZohoTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZohoTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZohoTestApplication.class, args);
+	}
+
+}
---
+++ ZohoTest/src/test/java/com/talina/ZohoTest/ZohoTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talina.ZohoTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZohoTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
